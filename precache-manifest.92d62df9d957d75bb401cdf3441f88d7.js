37b21ef003f038f413a2300d1db03041b0b4f7f4
Musham Srinivas Nagesh <srinu1349@gmail.com>
2023-03-10 12:47:43 +0530

    Updates

---
+++ precache-manifest.92d62df9d957d75bb401cdf3441f88d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "89a7d784593fd847fd3e",
+    "url": "/redux/static/css/main.751f140e.chunk.css"
+  },
+  {
+    "revision": "89a7d784593fd847fd3e",
+    "url": "/redux/static/js/main.f1344941.chunk.js"
+  },
+  {
+    "revision": "6e46d8687c460c1e2582",
+    "url": "/redux/static/js/runtime~main.f4e16322.js"
+  },
+  {
+    "revision": "f3afda117f246c896c6c",
+    "url": "/redux/static/js/2.ca48165b.chunk.js"
+  },
+  {
+    "revision": "ef276a8df7b31e3a5070",
+    "url": "/redux/static/js/3.d62f7fe7.chunk.js"
+  },
+  {
+    "revision": "00887c33453e748560ba0642f1be9152",
+    "url": "/redux/index.html"
+  }
+];
